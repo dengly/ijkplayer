284a6e6675f23afb56df25c3db151f658ccceb48
dengly <denglylove@gmail.com>
2019-11-06 14:48:57 +0800

    添加回c层软件解码的截图

--- ijkmedia/ijkplayer/android/ijkplayer_jni.c
+++ ijkmedia/ijkplayer/android/ijkplayer_jni.c
@@ -26,6 +26,7 @@
 #include <pthread.h>
 #include <jni.h>
 #include <unistd.h>
+#include <android/bitmap.h>
 #include "j4a/class/java/util/ArrayList.h"
 #include "j4a/class/android/os/Bundle.h"
 #include "j4a/class/tv/danmaku/ijk/media/player/IjkMediaPlayer.h"
@@ -360,6 +361,33 @@ LABEL_RETURN:
     ijkmp_dec_ref_p(&mp);
     return retval;
 }
+
+static jboolean
+IjkMediaPlayer_getCurrentFrame(JNIEnv *env, jobject thiz, jobject bitmap)
+{
+    jboolean retval = JNI_TRUE;
+    IjkMediaPlayer *mp = jni_get_media_player(env, thiz);
+    JNI_CHECK_GOTO(mp, env, NULL, "mpjni: getCurrentFrame: null mp", LABEL_RETURN);
+
+    uint8_t *frame_buffer = NULL;
+
+    if (0 > AndroidBitmap_lockPixels(env, bitmap, (void **)&frame_buffer)) {
+        (*env)->ThrowNew(env, "java/io/IOException", "Unable to lock pixels.");
+        return JNI_FALSE;
+    }
+
+    ijkmp_get_current_frame(mp, frame_buffer);
+
+    if (0 > AndroidBitmap_unlockPixels(env, bitmap)) {
+        (*env)->ThrowNew(env, "java/io/IOException", "Unable to unlock pixels.");
+        return JNI_FALSE;
+    }
+
+    LABEL_RETURN:
+    ijkmp_dec_ref_p(&mp);
+    return retval;
+}
+
 
 static jint
 IjkMediaPlayer_startRecord(JNIEnv *env, jobject thiz, jstring file)
@@ -1181,6 +1209,7 @@ static JNINativeMethod g_methods[] = {
     { "getCurrentPosition",     "()J",      (void *) IjkMediaPlayer_getCurrentPosition },
     { "getDuration",            "()J",      (void *) IjkMediaPlayer_getDuration },
 
+    { "getCurrentFrame",        "(Landroid/graphics/Bitmap;)Z",      (void *) IjkMediaPlayer_getCurrentFrame },
     { "startRecord",            "(Ljava/lang/String;)I",      (void *) IjkMediaPlayer_startRecord },
     { "stopRecord",             "()I",      (void *) IjkMediaPlayer_stopRecord },
 
