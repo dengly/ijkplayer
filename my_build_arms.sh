eac33833e5a4fd7bf29b8ee904d6dc33d6181a03
dengly <denglylove@gmail.com>
2019-11-08 19:26:51 +0800

    添加openssl的编译，支持https

--- my_build_arms.sh
+++ my_build_arms.sh
@@ -3,10 +3,22 @@
 # build ffmepg
 cd /Volumes/Data/Workspaces/topOpenSourceProjects/ijkplayer
 
+# 拉取必要的依赖库代码
 ./init-android.sh
 
+# 拉取可选依赖库代码
+./init-android-openssl.sh
+
+# 去到android编译目录
 cd android/contrib
+
+# 清除
+./compile-openssl.sh clean
 ./compile-ffmpeg.sh clean
+
+# 编译openssl
+./compile-openssl.sh arms
+# 编译ffmpeg
 ./compile-ffmpeg.sh arms
 
 cd ..
